d9d5d0b11864268478ff9b4d8775e3d5095c1abc
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2023-09-25 16:31:48 -0700

    re organized the repo a little

--- index.ts
+++ index.ts
@@ -1,21 +1,3 @@
-import { test_b_search } from "./BSearch";
-import { contains_node, validate_b_tree, validate_invalid_tree } from "./Btree";
-import { BTree } from "./Btree/ds";
-import { test_path_finder } from "./MazePath";
-import { example } from "./dmv";
-
-// dem_r_buf();
-// test_path_finder();
-// console.log("contains 25: ", contains_node(25));
-// console.log("contains 26: ", contains_node(26));
-
-// console.log("is valid tree: ", validate_b_tree());
-// console.log("is invalid tree: ", validate_invalid_tree());
-
-// const b_tree = new BTree();
-// b_tree.create_valid_b_tree();
-
-//const is_found = test_b_search();
-//console.log("is the number found", is_found);
+import { example } from "./problems/dmv";
 
 example();
